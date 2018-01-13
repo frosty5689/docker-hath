a6093ff9dcc3b040fb056d2d4a191ededf757cdd
frosty5689 <frosty5689@gmail.com>
2018-01-13 11:51:38 -0800

    Fix start.sh

--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@
 # Create client_login if it doesn't exist yet
 if [ ! -f /data/hath/data/client_login ]; then
 	echo "${HATH_CLIENT_ID}-${HATH_CLIENT_KEY}" >> /data/hath/data/client_login
-end
+fi
 
 java -jar /opt/hath/HentaiAtHome.jar  \
     --cache-dir=/data/hath/cache            \
