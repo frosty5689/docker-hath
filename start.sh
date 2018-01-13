274dad08d3f93fe02597015c8c91c630fd68c0da
frosty5689 <frosty5689@gmail.com>
2018-01-13 12:09:12 -0800

    Attempt to fix docker stop

--- start.sh
+++ start.sh
@@ -5,7 +5,7 @@ if [ ! -f /data/hath/data/client_login ]; then
 	echo "${HATH_CLIENT_ID}-${HATH_CLIENT_KEY}" >> /data/hath/data/client_login
 fi
 
-java -jar /opt/hath/HentaiAtHome.jar  \
+exec java -jar /opt/hath/HentaiAtHome.jar  \
     --cache-dir=/data/hath/cache            \
     --data-dir=/data/hath/data              \
     --download-dir=/data/hath/download      \
