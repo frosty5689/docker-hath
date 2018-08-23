7d8f128c838cace413a34e82096c9df2fa908df1
ScarletStar <stievu@yahoo.de>
2018-08-23 19:14:33 +0200

    Option to run with IPv4
    Adding option to run with IPv4 because IPv6 results in "Startup notification failed." sometimes

--- run/start.sh
+++ run/start.sh
@@ -5,10 +5,18 @@ if [ ! -f /hath/data/client_login ]; then
 	printf "${HATH_CLIENT_ID}-${HATH_CLIENT_KEY}" >> /hath/data/client_login
 fi
 
-exec java -jar /opt/hath/HentaiAtHome.jar \
-    --cache-dir=/hath/cache               \
-    --data-dir=/hath/data                 \
-    --download-dir=/hath/download         \
-    --log-dir=/hath/log                   \
-    --temp-dir=/hath/tmp
-
+if [ "$IPV4"=true ]; then
+	exec java -Djava.net.preferIPv4Stack=true -jar /opt/hath/HentaiAtHome.jar \
+    	    --cache-dir=/hath/cache               \
+            --data-dir=/hath/data                 \
+            --download-dir=/hath/download         \
+            --log-dir=/hath/log                   \
+            --temp-dir=/hath/tmp
+else	 
+	exec java -jar /opt/hath/HentaiAtHome.jar \
+	    --cache-dir=/hath/cache               \
+	    --data-dir=/hath/data                 \
+	    --download-dir=/hath/download         \
+	    --log-dir=/hath/log                   \
+	    --temp-dir=/hath/tmp
+fi
