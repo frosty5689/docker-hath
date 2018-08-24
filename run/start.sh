abffb2ffd18435a40ae88e9d659b093270f9a2dd
ScarletStar <stievu@yahoo.de>
2018-08-24 15:43:35 +0200

    simplified

--- run/start.sh
+++ run/start.sh
@@ -5,18 +5,10 @@ if [ ! -f /hath/data/client_login ]; then
 	printf "${HATH_CLIENT_ID}-${HATH_CLIENT_KEY}" >> /hath/data/client_login
 fi
 
-if [ "$IPV4"=true ]; then
-	exec java -Djava.net.preferIPv4Stack=true -jar /opt/hath/HentaiAtHome.jar \
-    	    --cache-dir=/hath/cache               \
-            --data-dir=/hath/data                 \
-            --download-dir=/hath/download         \
-            --log-dir=/hath/log                   \
-            --temp-dir=/hath/tmp
-else	 
-	exec java -jar /opt/hath/HentaiAtHome.jar \
-	    --cache-dir=/hath/cache               \
-	    --data-dir=/hath/data                 \
-	    --download-dir=/hath/download         \
-	    --log-dir=/hath/log                   \
-	    --temp-dir=/hath/tmp
-fi
+
+exec java -Djava.net.preferIPv4Stack=$PREFER_IPV4_STACK -jar /opt/hath/HentaiAtHome.jar \
+    --cache-dir=/hath/cache               \
+    --data-dir=/hath/data                 \
+    --download-dir=/hath/download         \
+    --log-dir=/hath/log                   \
+    --temp-dir=/hath/tmp
